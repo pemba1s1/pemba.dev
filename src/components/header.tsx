d2020713ee1cc535a8b64317a0bc89a8d1b36804
Pemba Sherpa <psherpa5121@conestogac.on.ca>
2025-05-26 02:53:29 -0400

    style(nav): make navigation bar sticky

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,7 +16,7 @@ function Header() {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0.8 * window.innerHeight) {
+      if (window.scrollY > 0.7 * window.innerHeight) {
         setIsFixed(true);
       } else {
         setIsFixed(false);
@@ -37,8 +37,8 @@ function Header() {
   };
   return (
     <div className='h-screen' id="home">
-      <header className="flex justify-between items-center pt-12 pb-7 text-4xl">
-        <div className="flex gap-2 text-neutral-500 hover:text-zinc-200 transition items-center">
+    <header className={`flex justify-between items-center h-[10vh] text-4xl z-20 ${isFixed ? 'fixed top-0 left-0 bg-zinc-950 px-20 w-full' : 'sticky top-0'}`}>
+        <div className="flex gap-2 text-neutral-500 hover:text-zinc-200 transition items-center cursor-pointer">
           <p className='font-bold text-3xl'>&lt;&gt;</p>
           <p>pemba.dev</p>
         </div>
@@ -63,7 +63,7 @@ function Header() {
           <p className="text-5xl font-bold">Pemba Norsang Sherpa</p>
         </div>
 
-        <div className={`flex gap-15 text-3xl justify-center w-full h-[10vh] items-center z-50 ${isFixed ? 'fixed top-0 px-20 bg-zinc-950' : 'sticky top-0'}`}>
+        <div className={`flex gap-25 text-3xl justify-center px-20 h-[10vh] items-center z-30 ${isFixed ? 'fixed top-0 bg-zinc-950' : 'sticky top-0'}`}>
           {Object.entries(navLinks).map(([path, label]) => (
             <button
               key={path}
